b6bef767ebca83516cd8c47c9cf6ff09b4f879c7
Dweirii <zaiddweiri962@gmail.com>
2025-11-18 15:08:18 +0400

    add the ai

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,13 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
 import { Toaster } from '@/components/ui/toaster'
+import Script from "next/script"
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Learnify',
-  description: 'Learnify LiveStream Platform for Live Learning, Project-Based Tracks, Gamification, and Community in one place to bridge the skill gap.',
+  description:
+    'Learnify LiveStream Platform for Live Learning, Project-Based Tracks, Gamification, and Community in one place to bridge the skill gap.',
   generator: 'Learnify',
   icons: {
     icon: '/favicon.ico',
@@ -16,11 +18,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://genie-ai-genesis-hackthon-widget.vercel.app/widget.js"
+          data-organization-id="org_35UIvA8d8LGBnfSLUvOazVwTDnG"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Toaster />
